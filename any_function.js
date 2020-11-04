a387f73fb7be8fb47ecdebe0318b4216fae4ed64
Maxim <robot3000dub@gmail.com>
2020-11-04 17:25:29 +0200

    Understanding of types functions and examples

---
+++ any_function.js
@@ -0,0 +1,18 @@
+/*console.log(sum(7,5));
+
+//function declaration
+function sum(num =0,num_1=0){
+    return num+num_1;
+}
+
+//function expression
+let Square = function(base,exp){
+    let b_new =1;
+    for(let i=1;exp>=i;i++){
+        let b_old = b_new;
+        b_new = b_old*base;
+    }
+    return b_new;
+}
+console.log(Square(2,5));
+*/
--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
+app.use(express.static('public'));
+
 app.listen(3000,function(){
     console.log('n-works');
 });
+
+app.get('/',function(req,res){
+    res.render('index.html');
+})
 app.get('/',function(req,res){
     res.end('<h1>hello</h1>');
 });
