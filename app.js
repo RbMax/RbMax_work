40e8d3b67e028a21809a708fba9f8f7bfc36d0fb
Maxim <robot3000dub@gmail.com>
2020-11-07 14:09:46 +0200

    adding bootstrap folder

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let arr = [2,3,'Hello','erunda'];
-console.log(arr);
+let express = require('express');
+let app = express();
+let port = 3000;
+app.listen(port,function(){
+    console.log('node express work on 3000');
+});
+
+app.get('/',function(request,response){
+    response.end('<h1>Main Page</h1> Super site');
+});
+
+app.get('/cat',function(request,response){
+    response.end('<h1>Category page</h1> <p>Any product</p>');
+});
