7f2460a552474870321bfc10b88ad89b1c2ef357
Maxim <robot3000dub@gmail.com>
2020-11-18 21:07:01 +0200

    finished product and header

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 let express = require('express');
 let app = express();
 let port = 3000;
-app.listen(port,function(){
-    console.log('node express work on 3000');
-});
+app.use(express.static('public'));
+//задаём шабланизатор
+//прописываем путь до папки
+//app.set('views','./views');
+//запуск шабланизатора
+app.set('view engine','pug');
 
 app.get('/',function(request,response){
-    response.end('<h1>Main Page</h1> Super site');
+  //  response.end('<h1>Main Page</h1> Super site');
+    response.render('index.pug');
 });
 
-app.get('/cat',function(request,response){
-    response.end('<h1>Category page</h1> <p>Any product</p>');
+app.listen(port,function(){
+    console.log('node express work on 3000');
 });
